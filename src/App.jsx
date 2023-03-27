17c32a6cdf143a2f4d56bca4ae9105473f02d304
Ult1mat3S <52617311+Ult1mat3S@users.noreply.github.com>
2023-03-27 03:16:06 -0600

    fix: copy netlify form

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,45 @@
+import { useState } from "react";
+
 export default function App() {
+  const [state, setState] = useState({});
+
+  const handleChange = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...state,
+      }),
+    })
+      .then(() => navigate(form.getAttribute("action")))
+      .catch((error) => alert(error));
+  };
+
   return (
     <div className="App">
       <h1>Feedback</h1>
-      <form name="contact" method="post" netlify netlify-honeypot="bot-field">
+      <form
+        name="contact"
+        method="post"
+        action="/thanks/"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        onSubmit={handleSubmit}
+      >
+        <input type="hidden" name="form-name" value="contact" />
+        <p hidden>
+          <label>
+            Test:
+            <input name="bot-field" onChange={handleChange} />
+          </label>
+        </p>
         <p>
           <input type="hidden" name="form-name" value="contact" />
           <input
---
+++ src/Thanks.jsx
@@ -0,0 +1,7 @@
+export default () => (
+  <div>
+    {" "}
+    <h1>Thank you!</h1>
+    <p>Your form has been sent successfully.</p>
+  </div>
+);
